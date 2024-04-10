f4dc6c5890f6bc57283581ddfb8bc623eb7f4595
jermaiine14 <jermainespam@gmail.com>
2024-04-10 15:43:09 +0200

    vogel paard panda

--- domeplease/Assets/Arduino.cs
+++ domeplease/Assets/Arduino.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
-using UnityEngine.SceneManagement;
 
 public class Arduino : MonoBehaviour
 {
 
     SerialPort data_stream = new SerialPort("COM3", 9600); 
     public string receivedstring;
-    public string sceneName;
+    public GameObject Horse;
+    public GameObject Bird;
     public int stringToInt;
+    private bool onOff;
     //public GameObject test_data;
     // Start is called before the first frame update
     void Start()
     {
+        onOff = true;
         data_stream.Open();
         
     }
@@ -25,8 +27,21 @@ public class Arduino : MonoBehaviour
         receivedstring = data_stream.ReadLine();
         int.TryParse(receivedstring, out stringToInt);
          if (stringToInt > 250){
-            SceneManager.LoadScene(sceneName);
-            receivedstring = null;
+            if (onOff == true){
+            onOff = false;
+            Vector3 randomSpawnPosition = new Vector3(0.67f, Random.Range(-0.005f,0.1f), Random.Range(-0.78f,0.62f));
+            Quaternion randomSpawnRotation = new Quaternion(-3, Random.Range(-30,-130), 8, 90);
+            Instantiate(Horse,randomSpawnPosition,randomSpawnRotation);
+            //receivedstring = null;
+            StartCoroutine(KillMe());
+            }
         }
+    }
+
+    IEnumerator KillMe()
+    {
+        yield return new WaitForSeconds(1);
+        onOff = true;
+        //Destroy(this.gameObject);
     }
 }
